13b41ebf1cf85051c70b2cff48185f6b8aa8259f
Torun1992 <ahossantorun@gmail.com>
2024-05-22 00:56:27 +0600

    [DONE] Student FEATURE

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { StudentRoutes } from "./app/modules/student/student.route";
 const app: Application = express();
 
 //parser
 app.use(express.json());
 app.use(cors());
+//application router
+app.use("/api/v1/students", StudentRoutes);
 
-app.get("/", (req: Request, res: Response) => {
+const getAController = (req: Request, res: Response) => {
   const a = 10;
-
   res.send(a);
-});
+};
+
+app.get("/", getAController);
 
 console.log(process.cwd());
 
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import config from "./app/config";
 
 async function main() {
   try {
-    await mongoose.connect(config.dattabase_url as string);
+    await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`App is listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
